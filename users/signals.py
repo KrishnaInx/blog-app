bd6ef69951520669ab84970ecfefa20e09940890
Krishna.inexture <krishnagabani.inexture@gmail.com>
2021-06-05 17:39:25 +0530

    adding profile

--- users/signals.py
+++ users/signals.py
@@ -8,8 +8,12 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        Profile.save()
+        instance.profile.save()
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
